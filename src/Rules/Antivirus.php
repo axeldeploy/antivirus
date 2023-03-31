b4a1f2f27e4425ed6ed1156d28a4c719f1de15ca
Nurislan Umetaliev <nurislan.umetaliev@axelkg.com>
2023-03-31 17:48:37 +0600

    first commit

---
+++ src/Rules/Antivirus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Axel\Antivirus\Rules;
+
+use Axel\Antivirus\Services\ClamAvService;
+use Illuminate\Contracts\Validation\Rule;
+
+class Antivirus implements Rule
+{
+    public function passes($attribute, $value): bool
+    {
+        $clamAvService = new ClamAvService();
+        return $clamAvService->infected($value);
+    }
+
+    public function message(): string
+    {
+        return "The :attribute contains virus";
+    }
+}
