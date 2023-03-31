221b2c2552ec6f54bb92f8f3e4ca8e772d7fc6f8
Nurislan Umetaliev <nurislan.umetaliev@axelkg.com>
2023-03-31 17:34:47 +0600

    first commit

---
+++ src/Services/ClamAvService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Axel\Antivirus\Services;
+
+use RuntimeException;
+use Socket;
+
+class ClamAvService
+{
+    public const CLAMAV_MAX = 20000;
+
+    protected function getSocket(): Socket
+    {
+        $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
+        $status = socket_connect($socket, '/var/run/clamav/clamd.ctl');
+        if (!$status) {
+            throw new RuntimeException('Unable to connect to ClamAV server');
+        }
+
+        return $socket;
+    }
+
+    public function infected(string $file): bool
+    {
+        $out = $this->sendCommand('SCAN ' . $file);
+        $out = \explode(':', $out);
+        $stats = \end($out);
+        return \trim($stats) !== 'OK';
+    }
+
+    public function ping(): bool
+    {
+        $return = $this->sendCommand('PING');
+        return \trim($return) === 'PONG';
+    }
+
+    public function version(): string
+    {
+        return \trim($this->sendCommand('VERSION'));
+    }
+
+    private function sendCommand(string $command): ?string
+    {
+        $return = null;
+
+        $socket = $this->getSocket();
+
+        \socket_send($socket, $command, \strlen($command), 0);
+        \socket_recv($socket, $return, self::CLAMAV_MAX, 0);
+        \socket_close($socket);
+
+        return \trim($return);
+    }
+}
